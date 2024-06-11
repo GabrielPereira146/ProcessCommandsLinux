3dc49947261901ce22923b02d9a8fbc660ae31e0
Gabriel Pereira <120065224+GabrielPereira146@users.noreply.github.com>
2024-06-10 23:29:45 -0300

    Feat - Features 7,8,9,10

--- Project2-Simulador-Sistema-Arquivos/FileSystemSimulator.c
+++ Project2-Simulador-Sistema-Arquivos/FileSystemSimulator.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 // Códigos de escape ANSI para cores e estilos
 #define RESET "\033[0m"
@@ -10,10 +11,16 @@
 #define GREEN "\033[32m"
 #define YELLOW "\033[93m"
 #define BLUE "\033[34m"
-#define MAGENTA "\033[35m"
+#define BLUE_LIGHT "\033[94m"
+#define BLUE_BRIGHT "\033[1;34m"
+#define BLUE_DARK "\033[34;1m"
+#define BLUE_VIVID "\033[94m"
 #define CYAN "\033[36m"
+#define MAGENTA "\033[35m"
 #define WHITE "\033[37m"
 //----------------------------------------------
+
+int remove_file(char *path);
 
 void showMenu(){
 	printf(BOLD CYAN "MENU\n");
@@ -103,36 +110,52 @@ void init_disk() {
 
 void create_directory(char *path) {
 	Entry *parent = &root_directory;
-
+  char *root = malloc(MAX_SIZE * sizeof(char));
+  strcpy(root,"root");
 	char *paths_dir[MAX_SIZE];
 	int num_dir = 0;
-
+  
+if(strncmp(path,"\\",1)==0){
+    strcat(root,path);
+    strcpy(path,root);
+  }else if(strncmp(path,"root\\",5)!=0){
+    strcat(root,"\\");
+    strcat(root,path);
+    strcpy(path,root);
+}
 	paths_dir[num_dir] = strtok(path, "\\");
 	while(paths_dir[num_dir] != NULL) {
 		paths_dir[++num_dir] = strtok(NULL, "\\");
 	}
+
 	
-	for (int i = 2; i < num_dir; i++) {
-		Entry *child = NULL;
+  for (int i = 2; i < num_dir; i++) {
+    Entry *child = malloc(sizeof(Entry));
+    child = NULL;
 		for (int j = 0; j < parent->quant_childs; j++) {
 			if (strcmp(paths_dir[i-1], parent->subdirectories[j]->name) == 0) {
-				child = parent->subdirectories[j];
+        if((parent->is_directory)==1)
+				  child = parent->subdirectories[j];
 				break;
 			}
 		}
+    
 		if (child == NULL ) {
 			printf(BOLD RED "Erro: O diretório pai especificado não existe.\n" RESET);
+      free(root);
+      free(child);
 			return; // Retorna sem criar o diretório
 		}
-		parent = child; // Atualiza o pai para o próximo diretório
-	}
+    parent = child; // Atualiza o pai para o próximo diretório
+  }
 
 	// Verifica se o diretório a ser criado já existe dentro do diretório pai
 	for (int j = 0; j < parent->quant_childs; j++) {
 		if (strcmp(paths_dir[num_dir - 1], parent->subdirectories[j]->name) == 0) {
 			printf(BOLD RED "Erro: O diretório a ser criado já existe no diretório pai.\n" RESET);
-			return; // Retorna sem criar o diretório
-		}
+			free(root);
+      return; // Retorna sem criar o diretório
+		} 
 	}
 
 	// Se chegou até aqui, o diretório pai existe e o diretório a ser criado não existe dentro dele
@@ -159,17 +182,34 @@ void create_directory(char *path) {
 	}
 	if(new_directory->start_sector == 0){
 		printf(BOLD RED "Erro: Sem espaço em disco para criar diretorio.\n" RESET);
-	}
+    return;	
+  }
+  free(root);
 	printf(BOLD GREEN "Diretório criado com sucesso.\n" RESET);
 }
 
 int remove_directory(char *path) {
 	Entry *parent = &root_directory;
-	char *path_sub = malloc(MAX_SIZE * sizeof(char));;
+	char *path_sub = malloc(MAX_SIZE * sizeof(char));
+  
+  char *root = malloc(MAX_SIZE * sizeof(char));
+  strcpy(root,"root");
+
+  if(strncmp(path,"\\",1)==0){
+    strcat(root,path);
+    strcpy(path,root);
+  }else if(strncmp(path,"root\\",5)!=0){
+    strcat(root,"\\");
+    strcat(root,path);
+    strcpy(path,root);
+  }
+  
 	strcpy(path_sub,path);
 	strcat(path_sub,"\\");
 	char *paths_dir[MAX_SIZE];
 	int num_dir = 0;
+  
+ 
 
 	paths_dir[num_dir] = strtok(path, "\\");
 	while(paths_dir[num_dir] != NULL) {
@@ -181,7 +221,8 @@ int remove_directory(char *path) {
 		child = NULL;
 		for (int j = 0; j < parent->quant_childs; j++) {
 			if (strcmp(paths_dir[i-1], parent->subdirectories[j]->name) == 0) {
-				child = parent->subdirectories[j];
+        if((parent->is_directory)==1)
+				   child = parent->subdirectories[j];
 				break;
 			}
 		}
@@ -191,7 +232,6 @@ int remove_directory(char *path) {
 		}
 		parent = child; // Atualiza o pai para o próximo diretório
 	}
-
 	// Verifica se o diretório a ser deletado existe dentro do diretório pai
 	char *aux = malloc(MAX_SIZE * sizeof(char)); //variavel para concatenar com o pathsub sem altera-lo
 	for (int j = 0; j < parent->quant_childs; j++) {
@@ -200,27 +240,465 @@ int remove_directory(char *path) {
 			child = parent->subdirectories[j];
 
 			for(int i = 0; i< child->quant_childs; i++){
-				fprintf(stderr,"%s",path_sub);
+				fprintf(stderr,"%s\n",path_sub);
 				strcpy(aux, path_sub);
 
 				strcat(aux,child->subdirectories[i]->name);
-
-				//remove_directory(aux);
+        if(child->subdirectories[i]->is_directory){
+          if(remove_directory(aux)){
+            return 1;
+          }
+        } else {
+          if(remove_file(aux)){
+            return 1;
+          }
+        }
+				
 			}
-		
-			parent->subdirectories[j] = parent->subdirectories[parent->quant_childs];
+		  bitmap[child->start_sector]=0;
+			parent->subdirectories[j] = parent->subdirectories[parent->quant_childs-1];
 			parent->quant_childs--;
+      free(root);
 			free(aux);
 			free(path_sub);
 			return 0;
 		}
 	}
-        free(aux);
+  free(root);
+  free(aux);
 	free(path_sub);
 	printf(BOLD RED "Erro: O diretório a ser deletado não existe no diretório pai.\n" RESET);
 	return 1; // Retorna sem deletar o diretório
 
 
+}
+
+void create_file(char *path, float size) {
+	Entry *parent = &root_directory;
+  char *root = malloc(MAX_SIZE * sizeof(char));
+  strcpy(root,"root");
+	char *paths_dir[MAX_SIZE];
+	int num_dir = 0;
+  
+if(strncmp(path,"\\",1)==0){
+    strcat(root,path);
+    strcpy(path,root);
+  }else if(strncmp(path,"root\\",5)!=0){
+    strcat(root,"\\");
+    strcat(root,path);
+    strcpy(path,root);
+}
+	paths_dir[num_dir] = strtok(path, "\\");
+	while(paths_dir[num_dir] != NULL) {
+		paths_dir[++num_dir] = strtok(NULL, "\\");
+	}
+
+	
+  for (int i = 2; i < num_dir; i++) {
+    Entry *child = malloc(sizeof(Entry));
+    child = NULL;
+		for (int j = 0; j < parent->quant_childs; j++) {
+			if (strcmp(paths_dir[i-1], parent->subdirectories[j]->name) == 0) {
+        if((parent->is_directory)==1)
+				  child = parent->subdirectories[j];
+				break;
+			}
+		}
+    
+		if (child == NULL ) {
+			printf(BOLD RED "Erro: O diretório pai especificado não existe.\n" RESET);
+      free(root);
+      free(child);
+			return; // Retorna sem criar o arquivo
+		}
+    parent = child; // Atualiza o pai para o próximo diretório
+  }
+
+	// Verifica se o diretório a ser criado já existe dentro do diretório pai
+	for (int j = 0; j < parent->quant_childs; j++) {
+		if (strcmp(paths_dir[num_dir - 1], parent->subdirectories[j]->name) == 0) {
+			printf(BOLD RED "Erro: O arquivo a ser criado já existe no diretório pai.\n" RESET);
+			free(root);
+      return; // Retorna sem criar o diretório
+		} 
+	}
+
+	// Se chegou até aqui, o diretório pai existe e o arquivo a ser criado não existe dentro dele
+	// Podemos criar o arquivo filho
+	Entry *new_file = (Entry *)malloc(sizeof(Entry));
+	strcpy(new_file->name, paths_dir[num_dir-1]); // Último elemento do caminho
+	new_file->start_sector = 0; // Defina o setor inicial adequadamente
+	new_file->size = size; // Defina o tamanho inicial adequadamente
+	new_file->is_directory = 0;
+	new_file->quant_childs = 0;
+	for (int k = 0; k < NUM_SECTORS; k++) {
+		new_file->subdirectories[k] = NULL;
+	}
+
+	// Adiciona o novo arquivo à lista de subdiretórios do pai
+	parent->subdirectories[parent->quant_childs++] = new_file;
+  double num_blocks = size/SECTOR_SIZE;
+  num_blocks = ceil(num_blocks);
+  int old_i = -1;
+  for(int j = 0; j<num_blocks; j++){
+    for(int i = 10; i < NUM_SECTORS; i++){
+      if(bitmap[i] == 0){
+        bitmap[i] = 1;
+        if(j==num_blocks-1){
+          fat[i] == -1;
+        }
+        if(j==0){
+          new_file->start_sector = i;
+        }else{
+          fat[old_i] = i;
+        }
+        old_i = i;
+        break;
+      }
+    }
+  }
+	if(new_file->start_sector == 0){
+		printf(BOLD RED "Erro: Sem espaço em disco para criar arquivo.\n" RESET);
+    return;	
+  }
+  free(root);
+	printf(BOLD GREEN "Arquivo criado com sucesso.\n" RESET);
+}
+
+int remove_file(char *path){
+	Entry *parent = &root_directory;
+	char *path_sub = malloc(MAX_SIZE * sizeof(char));
+  
+  char *root = malloc(MAX_SIZE * sizeof(char));
+  strcpy(root,"root");
+
+  if(strncmp(path,"\\",1)==0){
+    strcat(root,path);
+    strcpy(path,root);
+  }else if(strncmp(path,"root\\",5)!=0){
+    strcat(root,"\\");
+    strcat(root,path);
+    strcpy(path,root);
+  }
+  
+	strcpy(path_sub,path);
+	strcat(path_sub,"\\");
+	char *paths_dir[MAX_SIZE];
+	int num_dir = 0;
+  
+ 
+
+	paths_dir[num_dir] = strtok(path, "\\");
+	while(paths_dir[num_dir] != NULL) {
+		paths_dir[++num_dir] = strtok(NULL, "\\");
+	}
+	Entry *child = NULL;
+	fflush(stdout);
+	for (int i = 2; i < num_dir; i++) {
+		child = NULL;
+		for (int j = 0; j < parent->quant_childs; j++) {
+			if (strcmp(paths_dir[i-1], parent->subdirectories[j]->name) == 0) {
+        if((parent->is_directory)==1)
+				   child = parent->subdirectories[j];
+				break;
+			}
+		}
+		if (child == NULL ) {
+			printf(BOLD RED "Erro: O diretório pai especificado não existe.\n" RESET);
+			return 1; // Retorna sem deletar o diretório
+		}
+		parent = child; // Atualiza o pai para o próximo diretório
+	}
+	// Verifica se o arquivo a ser deletado existe dentro do diretório pai
+	char *aux = malloc(MAX_SIZE * sizeof(char)); //variavel para concatenar com o pathsub sem altera-lo
+	for (int j = 0; j < parent->quant_childs; j++) {
+
+		if (strcmp(paths_dir[num_dir - 1], parent->subdirectories[j]->name) == 0) {
+			child = parent->subdirectories[j];
+      int num_blocks = ceil((float)child->size / (float)SECTOR_SIZE);
+		  bitmap[child->start_sector]=0;
+       
+      int new_i = child->start_sector;
+      while (new_i != -1){
+       int temp = fat[new_i];
+       fat[new_i] = -1;
+       bitmap[new_i] = 0;
+       new_i = temp;
+      }
+			parent->subdirectories[j] = parent->subdirectories[parent->quant_childs-1];
+			parent->quant_childs--;
+      free(root);
+			free(aux);
+			free(path_sub);
+			return 0;
+		}
+	}
+  free(root);
+  free(aux);
+	free(path_sub);
+	printf(BOLD RED "Erro: O diretório a ser deletado não existe no diretório pai.\n" RESET);
+	return 1; // Retorna sem deletar o diretório
+}
+
+// int walkthroughTree(Entry *parent, int j){
+//   for (int i = 0; i < parent->quant_childs; i++) {
+//         walkthroughTree(parent->subdirectories[i],j);
+//   }
+//   j+=parent->quant_childs;
+//   return j;
+// }
+
+int walkthroughTree(Entry *parent, int j){
+  
+  for (int i = 0; i < parent->quant_childs; i++) {
+        j=+walkthroughTree(parent->subdirectories[i],j);
+  }
+  j+=parent->quant_childs;
+  return j;
+}
+
+void view_sector(char *path){
+  Entry *parent = &root_directory;
+  char *root = malloc(MAX_SIZE * sizeof(char));
+  strcpy(root,"root");
+	char *paths_dir[MAX_SIZE];
+	int num_dir = 0;
+  
+if(strncmp(path,"\\",1)==0){
+    strcat(root,path);
+    strcpy(path,root);
+  }else if(strncmp(path,"root\\",5)!=0){
+    strcat(root,"\\");
+    strcat(root,path);
+    strcpy(path,root);
+}
+	paths_dir[num_dir] = strtok(path, "\\");
+	while(paths_dir[num_dir] != NULL) {
+		paths_dir[++num_dir] = strtok(NULL, "\\");
+	}
+
+	
+  for (int i = 2; i < num_dir; i++) {
+    Entry *child = malloc(sizeof(Entry));
+    child = NULL;
+		for (int j = 0; j < parent->quant_childs; j++) {
+			if (strcmp(paths_dir[i-1], parent->subdirectories[j]->name) == 0) {
+        if((parent->is_directory)==1)
+				  child = parent->subdirectories[j];
+				break;
+			}
+		}
+    
+		if (child == NULL ) {
+			printf(BOLD RED "Erro: O diretório pai especificado não existe.\n" RESET);
+      free(root);
+      free(child);
+			return; // Retorna sem criar o arquivo
+		}
+    parent = child; // Atualiza o pai para o próximo diretório
+  }
+
+	// Procura o arquivo dentro do diretório pai
+	for (int j = 0; j < parent->quant_childs; j++) {
+		if (strcmp(paths_dir[num_dir - 1], parent->subdirectories[j]->name) == 0) {
+      int new_i = parent->subdirectories[j]->start_sector;
+      while (new_i != -1){
+        printf(BOLD " %d" RESET, new_i);
+        printf(BOLD CYAN " ➜ " RESET);
+        new_i = fat[new_i];
+      }
+      printf("NULL \n");
+      
+			free(root);
+		} 
+	}
+}
+
+int showDataInfo(char *path){
+  Entry *parent = &root_directory;
+  char *path_sub = malloc(MAX_SIZE * sizeof(char));
+  int count = 0;
+  char *root = malloc(MAX_SIZE * sizeof(char));
+  strcpy(root,"root");
+
+  if(strncmp(path,"\\",1)==0){
+    strcat(root,path);
+    strcpy(path,root);
+  }else if(strncmp(path,"root\\",5)!=0){
+    strcat(root,"\\");
+    strcat(root,path);
+    strcpy(path,root);
+  }
+
+  strcpy(path_sub,path);
+  strcat(path_sub,"\\");
+  char *paths_dir[MAX_SIZE];
+  int num_dir = 0;
+
+
+
+  paths_dir[num_dir] = strtok(path, "\\");
+  while(paths_dir[num_dir] != NULL) {
+    paths_dir[++num_dir] = strtok(NULL, "\\");
+  }
+  Entry *child = NULL;
+  fflush(stdout);
+  for (int i = 2; i < num_dir; i++) {
+    child = NULL;
+    for (int j = 0; j < parent->quant_childs; j++) {
+      if (strcmp(paths_dir[i-1], parent->subdirectories[j]->name) == 0) {
+        if((parent->is_directory)==1)
+          child = parent->subdirectories[j];
+        break;
+      }
+    }
+    
+    if (child == NULL ) {
+      printf(BOLD RED "Erro: O diretório pai especificado não existe.\n" RESET);
+      return 1; // Retorna sem deletar o diretório
+    }
+    
+    parent = child; // Atualiza o pai para o próximo diretório
+  }
+  strcpy(root,"root");
+  
+  if(strcmp(paths_dir[num_dir - 1],root) != 0){
+    for (int j = 0; j < parent->quant_childs; j++) {
+      if (strcmp(paths_dir[num_dir - 1], parent->subdirectories[j]->name) == 0) {
+        child = parent->subdirectories[j];
+        break;
+      }
+    }
+    parent = child;
+  }
+  
+  
+  if (!parent->is_directory){
+     printf(BOLD RED "Erro: O diretório a ser exibido não existe.\n" RESET);
+     return 1;
+  }
+  // Verifica se o diretório a ser deletado existe dentro do diretório pai
+    printf("Nome                            | Tipo       | Tamanho | Criado em           \n");
+    printf("-----------------------------------------------------------------------------\n");
+
+//   typedef struct Entry {
+// 	char name[100];
+// 	int start_sector;
+// 	int size;
+// 	int is_directory;
+// 	int quant_childs;
+// 	struct Entry *subdirectories[NUM_SECTORS];
+// } Entry;
+
+    char typeFile[12];
+
+    for (int j = 0; j < parent->quant_childs; j++) {
+        Entry *child = parent->subdirectories[j];
+        
+        // Imprimir o nome
+        printf("%-32s", child->name);
+        
+        // Imprimir o tipo
+        if (child->is_directory) {
+            strcpy(typeFile, "Diretório");
+            printf(BOLD CYAN "| %-11s " RESET, typeFile);
+        } else {
+            strcpy(typeFile, "Arquivo");
+            printf("| %-10s ", typeFile);
+        }
+        
+
+        // Imprimir o tamanho
+        printf("| %-7d ", child->size);
+
+        // Imprimir a data de criação
+        printf("| Data\n");
+    }
+    
+    
+//     for(int i = 0; i< child->quant_childs; i++){
+//       fprintf(stderr,"%s\n",path_sub);
+//       // 				strcpy(aux, path_sub);
+
+//       // 				strcat(aux,child->subdirectories[i]->name);
+//       // 				if(remove_directory(aux)){
+//       //           printf("ERRO ERRO ERRO\n");
+//       //           return 1;
+//       //         }
+//     }
+//     bitmap[child->start_sector]=0;
+//     printf("<removed> Posicao no Bitmap: %d Bitmap: %d\n", child->start_sector, bitmap[child->start_sector]);
+//     parent->subdirectories[j] = parent->subdirectories[parent->quant_childs-1];
+//     parent->quant_childs--;
+//     free(root);
+//     // 			free(aux);
+//     free(path_sub);
+//     return 0;
+  
+  count = walkthroughTree(&root_directory, 1);
+  printf("\nTotal de arquivos e diretórios %d\n", count);
+  count = 0;
+  for (int i = 0; i < NUM_SECTORS; i++) {
+    if (bitmap[i] == 1){
+      count++;
+    }
+  }
+  int TotalSize;
+  TotalSize = NUM_SECTORS * SECTOR_SIZE;
+  count = count * SECTOR_SIZE;
+  printf("Espaço ocupado %d bytes\n", count);
+  printf("Espaço livre %d bytes\n", (TotalSize-count));
+  free(root);
+  //   free(aux);
+  free(path_sub);
+  //printf(BOLD RED "Erro: O diretório a ser deletado não existe no diretório pai.\n" RESET);
+  return 1; // Retorna sem deletar o diretório
+
+}
+
+void showMap(){
+  for (int i = 0; i < NUM_SECTORS; i++) {
+    printf("|   Setor %3d   ", i);
+    if ((i + 1) % 16 == 0) {
+      printf("|\n");
+      for (int j = i - 15; j <= i; j++) {
+        if (bitmap[j] == 1) {
+          printf("|    Ocupado    ");
+        } else {
+          printf("|     Livre     ");
+        }
+      }
+      printf("|\n\n");
+    }
+  }
+
+
+}
+
+void showTree(Entry *parent, int depth, int isLast[]){
+  for (int i = 0; i < depth; i++) {
+        if (i < depth - 1) {
+            if (isLast[i]) {
+                printf("    ");
+            } else {
+                printf("│   ");
+            }
+        } else {
+            if (isLast[i]) {
+                printf("└── ");
+            } else {
+                printf("├── ");
+            }
+        }
+  }
+  if(parent->is_directory)
+    printf(BOLD CYAN "%s\n" RESET, parent->name);
+  else
+    printf("%s\n", parent->name);
+  for (int i = 0; i < parent->quant_childs; i++) {
+        isLast[depth] = (i == parent->quant_childs - 1);
+        showTree(parent->subdirectories[i], depth + 1, isLast);
+  }
 }
 
 int main(int argc, char **argv) {
@@ -250,7 +728,9 @@ int main(int argc, char **argv) {
 			else
 				printf(BOLD "Erro Sintaxe\n" RESET);
 		}else if (strcmp(args[0], "criaa") == 0){
-			printf(BOLD "Making file...\n" RESET);
+      if((args[1] != NULL)&&(args[2] != NULL)){
+        create_file(args[1], atof(args[2]));
+      }
 		}else if (strcmp(args[0], "removed") == 0){
 
 			if(args[1] != NULL){
@@ -259,8 +739,26 @@ int main(int argc, char **argv) {
 				}
 			}
 		}else if (strcmp(args[0], "removea") == 0){
-			printf(BOLD "Removing file...\n" RESET);
-		}else if (strcmp(args[0], "exit") == 0){
+			if(args[1] != NULL){
+        if(!remove_file(args[1])){
+          printf(BOLD GREEN "Arquivo deletado com sucesso.\n" RESET);
+        }
+      }
+		}else if (strcmp(args[0], "verd") == 0){
+			if(args[1] != NULL){
+        showDataInfo(args[1]);
+      }
+		}else if (strcmp(args[0], "verset") == 0){
+			if(args[1] != NULL){
+        view_sector(args[1]);
+      }
+		}else if(strcmp(args[0], "arvore") == 0){
+      int isLast[MAX_SIZE] = {0};
+      showTree(&root_directory,0,isLast);
+    }
+    else if(strcmp(args[0], "mapa") == 0){
+      showMap();
+    }else if (strcmp(args[0], "exit") == 0){
 			printf(BOLD "Closing the simulator...\n" RESET);
 			return EXIT_SUCCESS;			
 		}else if(strcmp(args[0], "help") == 0){
